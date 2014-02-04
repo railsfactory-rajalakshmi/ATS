136e34ad6fd5ac1ca3929730ca52f0df7d6e637a
railsfactory-rajalakshmi <rajalakshmis@railsfactory.org>
2014-02-04 11:11:41 +0530

    test case for inquirly for login page

---
+++ inquirly.rb
@@ -0,0 +1,20 @@
+require 'selenium-webdriver'
+page_name="login"
+File.exist?("./screen") ? FileUtils.rm_rf("./screen/.") : FileUtils.mkdir_p("./screen/")
+i=1
+path = "./screen"
+@browser = Selenium::WebDriver.for(:chrome)  
+@browser.get('http://inquirly2-new.railsfactory.com/')
+@browser.save_screenshot("#{path}/" + page_name + i.to_s + ".png")
+login=@browser.find_element(:class,"sign-in")
+login.click
+i=i+1
+user=@browser.find_element(:id,"exampleInputEmail2")
+user.send_keys("rajalakshmis@railsfactory.org")
+pwd=@browser.find_element(:id,"exampleInputPassword2")
+pwd.send_keys("sedin123")
+submit=@browser.find_element(:class,"signin_submit")
+@browser.save_screenshot("#{path}/" + page_name + i.to_s + ".png")
+submit.click
+i= i + 1
+@browser.save_screenshot("#{path}/" + page_name + i.to_s + ".png")
