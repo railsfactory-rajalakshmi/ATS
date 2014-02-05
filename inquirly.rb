fcad4bc6b4378fda455c25d1ed72df05d096ce8d
railsfactory-rajalakshmi <rajalakshmis@railsfactory.org>
2014-02-05 18:37:33 +0530

    Added nokogiri

--- inquirly.rb
+++ inquirly.rb
@@ -6,58 +6,89 @@ require 'nokogiri'
 class Inquirly
 
 def initialize
-@page_name="login"
-File.exist?("./screen") ? FileUtils.rm_rf("./screen/.") : FileUtils.mkdir_p("./screen/")
-@i=1
-@path = "./screen"
-@browser = Selenium::WebDriver.for(:chrome) 
-@doc = Nokogiri::HTML(open( "http://inquirly2-new.railsfactory.com/"))
+	File.exist?("./screen") ? FileUtils.rm_rf("./screen/.") : FileUtils.mkdir_p("./screen/")
+	@path = "./screen"
+	#will either work with chrome or firefox which ever driver is available
+	@browser = Selenium::WebDriver.for(:chrome) || Selenium::WebDriver.for(:firefox) 
+	@doc = Nokogiri::HTML(open( "http://inquirly2-new.railsfactory.com/"))
 end
-def get_url(url)
-@browser.get(url)
-@browser.save_screenshot("#{@path}/" + @page_name + @i.to_s + ".png")
-end
+
+def get_url(page_name,url)
+	 begin
+	#go to that particular url
+    @browser.get(url)
+	#to save the screen shot
+	  @browser.save_screenshot("#{@path}/" + page_name + ".png")
+	 rescue Selenium::WebDriver::Error::TimeOutError
+    exit(1)
+   end
+ end
+
 def get_link
-anchor = @doc.css('a')
-anchor.each{|href| puts href[:id]}
-
+	#to get all link in a html page
+	@a=[]
+	i=0
+	anchor = @doc.css('a')
+	p 'links available in this page is '
+	anchor.each{|href| 
+	puts href
+	}
+	p "input elements ids available are"
+	input_id=@doc.css('input')
+	input_id.each { |t| 
+ @a << t[:id]
+ }
+ for i in 0..@a.length
+ if(@a[i] != nil )
+	p @a[i] 
+ end
+ end
 end
 
-def login_page(user,pwd)
-login=@browser.find_element(:class,"sign-in")
-login.click
-@i=@i+1
-username=@browser.find_element(:id,"exampleInputEmail2")
-username.send_keys(user)
-password=@browser.find_element(:id,"exampleInputPassword2")
-password.send_keys(pwd)
-submit=@browser.find_element(:class,"signin_submit")
-@browser.save_screenshot("#{@path}/" + @page_name + @i.to_s + ".png")
-submit.click
-@i= @i + 1
-sleep 5
-@browser.save_screenshot("#{@path}/" + @page_name + @i.to_s + ".png")
-sleep 2
-arrow=@browser.find_element(:class,"arrow-down")
-arrow.click
+#to fill the login details
+def login_page(page_name,user,pwd)
+	begin
+		i=1
+		login=@browser.find_element(:class,"sign-in")
+		login.click
+		username=@browser.find_element(:id,@a[2])
+		username.send_keys(user)
+		password=@browser.find_element(:id,@a[3])
+		password.send_keys(pwd)
+		submit=@browser.find_element(:class,"signin_submit")
+		@browser.save_screenshot("#{@path}/" + page_name + i.to_s + ".png")
+		submit.click
+		i= i + 1
+		sleep 5
+		@browser.save_screenshot("#{@path}/" + page_name + i.to_s + ".png")
+		sleep 2
+		arrow=@browser.find_element(:class,"arrow-down")
+		arrow.click
+	rescue Selenium::WebDriver::Error::TimeOutError
+	  exit(1)
+  end
 end
-def logout
-logout=@browser.find_element(:xpath,"/html/body/div/div/div[2]/div/a[2]")
-logout.click
+	#logout the page 
+def logout(page_name)
+	logout=@browser.find_element(:xpath,"/html/body/div/div/div[2]/div/a[2]")
+	logout.click
 end
+
 def close_browser
-@browser.close
+ @browser.close
 end
 end
-
+#I have included url, user, password in value.xls file
 oo = Roo::Excel.new("value.xls")
 oo.default_sheet = oo.sheets.first
-url =oo.cell(1,1)
-user = oo.cell(2,1)
-pwd=oo.cell(3,1)
+#to fetch the values from the xls file
+url =oo.cell(1,2)
+user = oo.cell(2,2)
+pwd=oo.cell(3,2)
 obj=Inquirly.new
-obj.get_url(url)
-obj.login_page(user,pwd)
-obj.logout
+obj.get_link
+obj.get_url('url',url)
+obj.login_page('login',user,pwd)
+obj.logout('logout')
 obj.close_browser
 
--- sbtn.rb
+++ sbtn.rb
@@ -1,17 +1,21 @@
-= begin
+=begin
 30.01.2014
 Today I have tried with roo gem since It could work with google doc,excel and also in ods files,
 Also I have take screen shots of the files, I have run the code for sbtn browser with chrome browser
 =end
-
+require 'ostruct'
 require 'selenium-webdriver'
 require 'roo'
+require 'open-uri'
+require 'nokogiri'
+
 class Test
 
 File.exist?("./tmp") ? FileUtils.rm_rf("./tmp/.") : FileUtils.mkdir_p("./tmp/")
 $temp_path = "./tmp"
 def initialize
  @browser = Selenium::WebDriver.for(:chrome)  
+ @doc = Nokogiri::HTML(open("http://localhost:3000/contacts/new"))
 end
 
 def login(uname,pw, page_name)
@@ -32,22 +36,40 @@ button = @browser.find_element(:id , 'user_submit')
 button.click
 end
 
-def contact_us(page_name)
- contact= @browser.find_element(:xpath,"/html/body/div/ul/li[2]/a")
- contact.click
- name=@browser.find_element(:id,"contact_name")
- name.send_keys('Test')
- contact_company_name=@browser.find_element(:id,"contact_company_name")
- contact_company_name.send_keys('Test')
- contact_email= @browser.find_element(:id,"contact_email")
- contact_email.send_keys('Test@gmail.com')
- contact_mobile_number=@browser.find_element(:id,"contact_mobile_number")
- contact_mobile_number.send_keys('987654321')
- contact_subject=@browser.find_element(:id,"contact_subject")
- contact_subject.send_keys('Test')
- contact_description=@browser.find_element(:id,"contact_description")
- contact_description.send_keys('This is a test for ats')
- submit =@browser.find_element(:id , "contact_submit")
+def contact_us(page_name,contact)
+ a=[]
+ input_id = @doc.css('input')
+ text_area =@doc.css('textarea')
+  text_area.each { |ta| 
+ p "ID OF TEXTAREA =" + ta[:id] 
+ }
+ input_id.each { |t| 
+ a << t[:id]
+ }
+ len=a.length
+ i=0
+ for i in 0..len
+  if(a[i] != nil )
+   p a[i]
+  end
+ end
+ p a.length
+
+ contact_link= @browser.find_element(:xpath,"/html/body/div/ul/li[2]/a")
+ contact_link.click
+ cname=@browser.find_element(:id,a[2])
+ cname.send_keys(contact.name)
+ contact_company_name=@browser.find_element(:id,a[3])
+ contact_company_name.send_keys(contact.company_name)
+ contact_email= @browser.find_element(:id,a[4])
+ contact_email.send_keys(contact.email)
+ contact_mobile_number=@browser.find_element(:id,a[5])
+ contact_mobile_number.send_keys(contact.mobile_number)
+ contact_subject=@browser.find_element(:id,a[6])
+ contact_subject.send_keys(contact.subject)
+ contact_description=@browser.find_element(:id,'contact_description')
+ contact_description.send_keys(contact.description)
+ submit =@browser.find_element(:id , a[7])
 @browser.save_screenshot("#{$temp_path}/" + page_name + ".png")
  submit.click
 end
@@ -63,7 +85,14 @@ oo.default_sheet = oo.sheets.first
 @pwd = oo.cell(2,1).to_i
 obj = Test.new
 obj.login(@uname,@pwd, "login")
-obj.contact_us("contact")
+@contact = OpenStruct.new
+@contact.name='Test'
+@contact.company_name='Test company'
+@contact.email='Test@gmail.com'
+@contact.mobile_number='987654321'
+@contact.subject='Test subject'
+@contact.description='Test Description for sbtn app'
+obj.contact_us("contact",@contact)
 
 
 
